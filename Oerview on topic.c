805734c4d7717176360d9a04e69563f9c0df6f8c
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2023-04-20 11:46:25 +0600

    Add files via upload

---
+++ Oerview on topic.c
@@ -0,0 +1,27 @@
+/*......Rabbi Hasan.....*/
+/*..University of BUBT..*/
+/*.....Dept. of CSE.....*/
+
+    Overview on this topic
+    ----------------------
+1.  Primality test
+2.  Sive of eratosthenes
+3.  Prime Factorization
+4.  Prime number of Divisor
+5.  Finding Number Of divisor of N
+6.  Binary exponential
+7.  Matrix exponential
+8.  Euclide Algorithm for GCD
+9.  Matrix GCD
+10. Segmented Sieve
+11. Calculating Binomial Coefficient
+12. Extended Euclidean Algorithm
+13. Modular Arthmcetic***
+14. Linear Diphantine Equation
+15. Fionacci Number in o(logn)
+16. Chinese Remainder Theorem
+17. Euler Totient function
+18. Sum of divisor
+19. Non Deterministic Primality test
+20. Fermat Primality test
+21. Miller-Rabin primality test
