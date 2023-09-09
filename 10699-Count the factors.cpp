f2776bf1ab596d01719db0911f96f82c351ef878
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2023-09-10 02:00:13 +0600

    UVA problem

---
+++ 10699-Count the factors.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include <math.h>
+#define fast() ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#include<vector>
+#define ll long long
+#define lli long long int
+#define nn "\n"
+using  namespace  std;
+/*  let's started  */
+
+int main()
+{
+    fast();
+    int n;
+    while(scanf("%d", &n) == 1 && n)
+    {
+        cout<<n<<" : ";
+        int ans=0;
+        for(int i=2; i*i<=n; i++)
+        {
+
+            if(n%i==0)
+            {
+              ans++;
+              while(n%i==0) n/=i;
+            }
+        }
+        if(n>1)ans++;
+        cout<<ans<<nn;
+    }
+    return 0;
+}
