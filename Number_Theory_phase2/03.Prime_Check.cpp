44395cddb6596ee70c33909883468f613f2c1bdc
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2024-02-04 02:10:30 +0600

    Prime_Check in [ log(n) ]

---
+++ Number_Theory_phase2/03.Prime_Check.cpp
@@ -0,0 +1,28 @@
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+bool isPrime(int n){
+  if(n % 2 == 0 && n != 2) return false;
+    for(int i = 2; i * i <= n; i += 2){
+        if(n % i == 0){
+          return false;
+        }
+    }
+  return true;
+}
+
+int32_t main(){
+  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+  int t = 1;//cin >> t;
+  while(t--){
+    int n;cin >> n;
+    if(isPrime(n)){
+      cout << "Prime..\n";
+    }else{
+      cout << "Not Prime..\n";
+    }
+  }
+  return 0;
+}
