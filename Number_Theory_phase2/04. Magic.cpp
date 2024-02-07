a0bf5d5b436e6849df0a3899fea8d15d96730d95
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2024-02-08 02:21:23 +0600

    10^18 prime_factor in O[sqrt(n)]

---
+++ Number_Theory_phase2/04. Magic.cpp
@@ -0,0 +1,31 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n;
+    cin >> n;
+    
+    vector<ll> factors;
+    for (ll i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            factors.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1) {
+        cout << n << '\n';
+    }else{
+    	cout << factors[factors.size() - 1] << '\n';
+    }
+
+    // for (auto factor : factors) {
+    //     cout << factor << '\n';
+    // }
+
+    return 0;
+}
