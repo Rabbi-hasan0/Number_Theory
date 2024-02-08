a5c4d49c32fd6d74c8484312b4d3caaf2cda49ce
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2024-02-09 02:25:11 +0600

    Divisor-O(sqrt(n))

--- Number_Theory_phase2/02. Divisor-O(sqrt(n)).cpp
+++ Number_Theory_phase2/02. Divisor-O(sqrt(n)).cpp
@@ -9,10 +9,13 @@ int32_t main(){
   int t = 1;//cin >> t;
   while(t--){
     int n;cin >> n;
-
-    for(int i = 1; i <= n; i++){
+    
+    for(int i = 1; i * i <= n; i++){
       if(n % i == 0){
         cout << i << ' ';
+        if(i != n / i){
+          cout << n / i << ' ';
+        }
       }
     }
   }
